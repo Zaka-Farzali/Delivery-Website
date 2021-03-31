eb98c45f1d2940637c8fd99cf6302058b4fb4257
Zaka Farzali <zeka.farzali@gmail.com>
2021-03-31 15:08:27 +0200

    custom user authentication model added

--- deliveryapp/admin.py
+++ deliveryapp/admin.py
@@ -1,14 +1,75 @@
-
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.models import Group
+from django.core.exceptions import ValidationError
+
 from .models import *
 # Register your models here.
 
+class UserCreationForm(forms.ModelForm):
+
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Customer
+        fields = ('email',)
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 
-admin.site.register(Customer)
+class UserChangeForm(forms.ModelForm):
 
-# admin.site.register(Customer)
-# admin.site.register(FAQ)
-# admin.site.register(Order)
-# admin.site.register(Partner)
-# admin.site.register(Storage)
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = Customer
+        fields = ('email', 'name','surname','birthday','phone_number','address', 'is_staff', 'is_active', 'is_superuser')
+    def clean_password(self):
+
+        return self.initial["password"]
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+
+    list_display = ('email', 'name', 'surname', 'birthday', 'phone_number', 'address', 'registration_date')
+    list_filter = ('is_staff','email', 'birthday', 'registration_date')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name','surname','address','birthday')}),
+        ('Permissions', {'fields': ('is_staff',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'surname', 'address', 'birthday', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('is_staff','email', 'birthday', 'registration_date',)
+    ordering = ('is_staff','email', 'birthday', 'registration_date',)
+    filter_horizontal = ()
+
+      
+admin.site.register(Customer,UserAdmin)
+admin.site.unregister(Group)
+admin.site.register(FAQ)
+admin.site.register(Order)
+admin.site.register(Partner)
+admin.site.register(Storage)
--- deliveryapp/models.py
+++ deliveryapp/models.py
@@ -3,46 +3,95 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.db.models.deletion import CASCADE
 from django.utils import timezone
 from django.utils.translation import gettext_lazy
+from django.contrib.auth.hashers import make_password
 # Create your models here.
 
 class UserAccountManager(BaseUserManager):
-    def create_superuser(self, Email, Name, Surname, Password = None, **other_fields):
+    def create_user(self, email, password = None, **other_fields):
         
-        other_fields.setdefault('is_staff', True)
-        other_fields.setdefault('is_superuser', True)
-        other_fields.setdefault('is_active', True)
-        return self.create_user(Email, Name = Name, Surname = Surname, Password = Password, **other_fields)
-
-
-    def create_user(self, Email, Name, Surname, Password = None, **other_fields):
-        
-        if not Email:
+        if not email:
             raise ValueError(gettext_lazy('You must provide email address'))
 
-        email = self.normalize_email(Email)
-        user = self.model(email, Name, Surname, **other_fields)
-        user.set_password(Password)
+        email = self.normalize_email(email)
+        user = self.model(email=email , **other_fields)
+        user.password  = make_password(password)
         user.save(using=self._db)
        
         return user
 
+    def create_superuser(self, email, password = None, **other_fields):
+        
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+        return self.create_user(email=email, password = password, **other_fields)
+
 class Customer(AbstractBaseUser, PermissionsMixin):
-    Email = models.EmailField(gettext_lazy('email address'), max_length=256, unique=True)
-    Name = models.CharField(max_length=64)
-    Surname = models.CharField(max_length=64, null=True)
-    Birthday = models.DateField(auto_now=False, null=False,blank=True)
-    PhoneNumber = models.CharField(max_length=16, unique=True)
-    Address = models.CharField(max_length=128)
-    RegistrationDate  = models.DateTimeField(default=timezone.now, editable=False)
+    email = models.EmailField(gettext_lazy('email address'), max_length=256, unique=True)
+    name = models.CharField(max_length=64, null=True)
+    surname = models.CharField(max_length=64, null=True)
+    birthday = models.DateField(auto_now=False, null=True,blank=True)
+    phone_number = models.CharField(max_length=16, unique=True, null=True, blank=True)
+    address = models.CharField(max_length=128, blank=True)
+    registration_date  = models.DateTimeField(default=timezone.now, editable=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     objects = UserAccountManager()
-    USERNAME_FIELD = 'Email'
-    REQUIRED_FIELDS = ['Name', 'Surname']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return self.Name + " " + self.Surname
+        return self.email
 
     def has_perm(self, perm, obj = None):
         return self.is_superuser
+
+
+class FAQ(models.Model):
+    Question = models.TextField(max_length=512)
+    Answer = models.TextField()
+    Date = models.DateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.Question
+
+class Partner(models.Model):
+    Name = models.CharField(max_length=64)
+    Logo = models.ImageField()
+    Url = models.URLField()
+    Description = models.TextField(max_length=256)
+    Date = models.DateTimeField(default=timezone.now, editable=False)
+
+    def __str__(self):
+        return self.Name
+
+class Order(models.Model):
+
+    STATUS_CHOICES =[
+        ('PR', 'In Progress'),
+        ('OR','Ordered'),
+        ('DL','Delivered'),
+        ('FN','Finished'),
+    ]
+    Url = models.URLField()
+    Color = models.CharField(max_length=32)
+    Size = models.CharField(max_length=16)
+    Amount = models.IntegerField()
+    OtherInfo = models.TextField(max_length=256)
+    Status  = models.CharField(max_length=2, choices=STATUS_CHOICES,default='PR', editable=True)
+    OrderDate = models.DateTimeField(default=timezone.now, editable=False)
+    Customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Url 
+
+class Storage(models.Model):
+    Country = models.CharField(max_length=32)
+    City = models.CharField(max_length=32)
+    Address = models.CharField(max_length=128)
+    Date = models.DateTimeField(default=timezone.now, editable=False)
+    Partner = models.ManyToManyField(Partner)
+
+    def __str__(self):
+        return self.City + " " + self.Address
