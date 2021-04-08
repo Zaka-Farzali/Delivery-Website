88b9d49ca4ad762c7c94856bb387fdbc99c5bb44
Zaka Farzali <zeka.farzali@gmail.com>
2021-04-09 00:54:48 +0200

    fix minor errors

--- delivery/settings.py
+++ delivery/settings.py
@@ -111,8 +111,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# MEDIA_URL ='/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 AUTH_USER_MODEL = 'deliveryapp.Customer'
 
--- delivery/urls.py
+++ delivery/urls.py
@@ -1,14 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
-from deliveryapp import views 
+from deliveryapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
-    path('aboutus/',views.aboutus, name='aboutus'),
+    path('', views.home),
+    path('aboutus/', views.aboutus, name='aboutus'),
     path('signin/', views.signin_user, name='sigin'),
-    path('signup/', views.signup_user , name='signup'),
+    path('signup/', views.signup_user, name='signup'),
     path('signout/', views.signout_user, name='signout'),
     path('order/', views.order, name='order'),
     path('products/', views.products, name='products'),
