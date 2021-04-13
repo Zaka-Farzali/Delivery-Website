080f00e42839e10444a3928fce1c35aa35739b77
Zaka Farzali <zeka.farzali@gmail.com>
2021-04-13 23:11:29 +0200

    change db to postgresql

--- delivery/settings.py
+++ delivery/settings.py
@@ -66,11 +66,14 @@ WSGI_APPLICATION = 'delivery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'delivery',
+        'USER': 'zaka',
+        'PASSWORD': 'F147741zs!',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
